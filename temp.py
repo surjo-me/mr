f83ef0ba94703259f0be38ec900f10acfb8771c4
surjo-me <116312411+surjo-me@users.noreply.github.com>
2022-10-21 02:03:01 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,11 @@
+# def factorial(n):
+#     if n==0 or n==1:
+#         return 1
+#     else:
+#         return n*factorial(n-1)
+# i=int(input("put the number"))
+# print(factorial(i))
+
+cat="Not  Human"
+c=list(cat)
+print(c)
